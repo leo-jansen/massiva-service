e7e76b912bffbbb1af31e8a06de0857eae2dfd5f
unknown <Administrador@TRJ3589074.wks.internal.timbrasil.com.br>
2022-12-22 08:37:30 -0300

    TODO

--- src/main/java/br/com/timbrasil/portalcop/massivaservice/util/NotificacaoMassiva.java
+++ src/main/java/br/com/timbrasil/portalcop/massivaservice/util/NotificacaoMassiva.java
@@ -2,7 +2,7 @@ package br.com.timbrasil.portalcop.massivaservice.util;
 
 public class NotificacaoMassiva {
   public static boolean enviar(Long id, String tipo)  {
-
+    // TODO: fazer envio de email
     return true;
   }
 }
